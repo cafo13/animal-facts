bee350909610fb118edc2f1e804816c58f81b369
cafo13 <yannik.foth97@gmail.com>
2023-12-19 23:02:55 +0100

    revert admin database ping

--- pkg/repository/facts_repository.go
+++ pkg/repository/facts_repository.go
@@ -51,11 +51,6 @@ func NewMongoDBFactsRepository(mongoDbUri string) (FactsRepository, error) {
 		return nil, err
 	}
 
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-		return nil, errors.Wrap(err, "failed to ping mongo db")
-	}
-	log.Logger().Info("connected to mongo db")
-
 	databaseName := "animal-facts"
 
 	databaseNameFromEnv, exists := os.LookupEnv("MONGODB_DATABASE_NAME")
@@ -64,6 +59,10 @@ func NewMongoDBFactsRepository(mongoDbUri string) (FactsRepository, error) {
 	}
 
 	log.Logger().Info("using database: " + databaseName)
+	if err := client.Database(databaseName).RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		return nil, errors.Wrap(err, "failed to ping mongo db")
+	}
+	log.Logger().Info("connected to mongo db")
 
 	return &MongoDBFactsRepository{client, databaseName}, nil
 }
